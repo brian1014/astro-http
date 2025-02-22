66e3071763fe2bf2e0e939cecf128d21c4dc6913
Brian Alvarez <brianalvarez2000@gmail.com>
2025-02-21 22:41:46 -0500

    change ssr adapter to cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+
   // output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  integrations: [mdx(), sitemap()],
+
+  adapter: cloudflare(),
 });
